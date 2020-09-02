f7f18d37b764c8a2b56390bec43f00e46b4a9167
sdburt <seandburt@gmail.com>
2020-09-02 11:01:20 -0700

    Add navigation items

--- burger-app/src/components/Navigation/NavigationItems/NavigationItems.js
+++ burger-app/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import NavigationItem from './NavigationItem/NavigationItem'
+
+import classes from './NavigationItems.module.css'
+
 const NavigationItems = props => {
     return (
-        <ul>
-            <li>
-                <a href="/">A Link</a>
-            </li>
+        <ul className={classes.NavigationItems}>
+            <NavigationItem link="/" active>Burger Builder</NavigationItem>
+            <NavigationItem link="/">Checkout</NavigationItem>
         </ul>
     )
 }
